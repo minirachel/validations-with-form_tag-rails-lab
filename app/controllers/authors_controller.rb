da173eb1169258cb6a8e061788e94006a10aea49
minirachel <rachel.a.levinson@gmail.com>
2021-08-12 11:22:35 -0400

    commit tree dec637e1e95b023cfe4f9a5af027cfe675b61f76

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,32 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+  def create
+    @author = Author.new(author_params)
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+    
   end
 
   private
